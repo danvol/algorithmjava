bb8d8581604b3d8aac9c02b798b9c0d26053a2a4
zxjlfx <zxjlfx@gmail.com>
2019-12-05 15:36:15 +0800

    修改了

--- src/com/zxj/graph/Point.java
+++ src/com/zxj/graph/Point.java
@@ -4,8 +4,8 @@ package com.zxj.graph;
  * 地图点
  */
 public class Point {
-    int x;//横坐标
-    int y;//纵坐标
+    public int x;//横坐标
+    public int y;//纵坐标
 
     public Point(int x, int y) {
         this.x = x;
